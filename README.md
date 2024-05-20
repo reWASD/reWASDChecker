4f891222f6a613ba715958be8b79db74eb602495
eagle <eagle@foxportal.com>
2024-05-20 11:04:39 +0300

    Updated reWASDChecker for reWASD 7.3

--- README.md
+++ README.md
@@ -6,12 +6,9 @@ A straightforward tool for quickly checking some features using with reWASD. The
 * Some profile with virtual gamepad is active
   * also mouse movement mapping is present or not present
 
-If the reWASD version is below 7.1, some flags may not be supported, in which case it is assumed that feature could be used.
-
 The source code can be fully adapted to suit individual needs.
-
 
 ## Requirements
 
 * Visual Studio 2022
-* reWASD 7.1 or higher
+* reWASD 7.3 or higher
--- reWASDChecker/reWASDChecker.cpp
+++ reWASDChecker/reWASDChecker.cpp
@@ -11,37 +11,27 @@
 #include <tchar.h>
 #include <stdio.h>
 
-#define REWASD_PIPE_NAME _T("\\\\.\\pipe\\reWASDService")
+#define REWASD_PIPE_NAME _T("\\\\.\\pipe\\{C65657D6-B9FA-40E9-B95E-6F3BDED9EFE4}")
 
 //Values for ServiceFlags
 //It is recommended for application to check these flags periodically, for example once per minute,
 //because user may apply different profiles or switch slots on the fly.
 //Also the service itself may be stopped and restarted.
-//If service version is too old and does not support some flag then application may treat it as incompatible and disable
-//some functionality. i.e. treat it as if the flag is set.
 
 //This flag indicates that turbo or some other function is enabled, which may be considered by game as a cheat.
 //This flag was added in service version 1.39.
 #define REWASD_SERVICE_FLAG_CHEATS_ENABLED                  0x0001
 
-#define REWASD_SERVICE_FLAG_CHEATS_ENABLED_MAJOR_VERSION    1
-#define REWASD_SERVICE_FLAG_CHEATS_ENABLED_MINOR_VERSION    39
-
 //This flag indicates that virtual gamepad is enabled.
-//Some application mayb not support virtual gamepads at all.
+//Some application may not support virtual gamepads at all.
 //But application can also check REWASD_SERVICE_FLAG_MOUSE_TO_GAMEPAD_ENABLED to verify if user uses mouse and add
 //restriction only for this case (eg. disable Aim Assist only).
-//This flag was added in service version 5.28.
 #define REWASD_SERVICE_FLAG_VIRTUAL_GAMEPAD_ENABLED         0x0002
 
 //This flag indicates that mapping of mouse movement to virtual gamepad is enabled,
 //and is used with REWASD_SERVICE_FLAG_VIRTUAL_GAMEPAD_ENABLED only.
 //For such case some games may not provide Aim Assist.
-//This flag was added in service version 5.28.
 #define REWASD_SERVICE_FLAG_MOUSE_TO_GAMEPAD_ENABLED        0x0004
-
-#define REWASD_SERVICE_FLAG_VIRTUAL_AND_MOUSE_MAJOR_VERSION 5
-#define REWASD_SERVICE_FLAG_VIRTUAL_AND_MOUSE_MINOR_VERSION 28
 
 typedef struct _REWASD_CHECK_VERSION_REQUEST{
     //Must be size of this structure, i.e. 8
@@ -71,166 +61,81 @@ int main()
 {
     DWORD err;
 
-    //Try to open communication pipe with reWASD service.
-    HANDLE hPipe = INVALID_HANDLE_VALUE;
-
-    ULONG NumRetries = 3;
-
-    while (NumRetries--)
-    {
-        hPipe = CreateFile(REWASD_PIPE_NAME,
-                           GENERIC_READ | GENERIC_WRITE,
-                           FILE_SHARE_READ | FILE_SHARE_WRITE,
-                           NULL,           // default security attributes
-                           OPEN_EXISTING,
-                           0,
-                           NULL);
-
-        // Break if the pipe handle is valid.
-        if (hPipe != INVALID_HANDLE_VALUE)
-        {
-            break;
-        }
-
-        err = GetLastError();
-
-        if (err == ERROR_ACCESS_DENIED)
-        {
-            break;
-        }
-
-        if (NumRetries)
-        {
-            Sleep(10);
-        }
-    }
-
-    if (hPipe == INVALID_HANDLE_VALUE)
-    {
-        //Unable to open pipe - seems service is not running
-        printf("Unable to connect to reWASD service, error %d!\n", err);
-
-        return err;
-    }
-
-    printf("Connected to reWASD service.\n");
-
-    //At this point application knows that reWASD service is running and may decide to inform the user
-    //that remapper is active and some functionality may be disabled.
-    //In this case no more checks are needed.
-    //But it is highly recommended to also query reWASD service version and check which functionality is used to make better decision
-    //and not try to restrict the user who may need remapper for legitimate purposes (disabled users etc).
-
-    //Change pipe to message-read mode.
-    DWORD dwMode = PIPE_READMODE_MESSAGE;
-
-    if (!SetNamedPipeHandleState(hPipe,
-                                 &dwMode, // new pipe mode
-                                 NULL,    // don't set maximum bytes
-                                 NULL))   // don't set maximum time
-    {
-        err = GetLastError();
-
-        printf("SetNamedPipeHandleState failed, error %d!\n", err);
-
-        CloseHandle(hPipe);
-
-        return err;
-    }
-
     //Check service version
     REWASD_CHECK_VERSION_REQUEST CheckVersionRequest = {sizeof(CheckVersionRequest), 0};
     UCHAR OutBuffer[256];
     DWORD BytesRead = 0;
 
+    //Upper 8 bits of Size are used as command Tag starting from reWASD 7.3
+    CheckVersionRequest.Size |= 1UL << 24;//set tag to any non-zero value
+
     //Clear output buffer
     memset(OutBuffer, 0, sizeof(OutBuffer));
 
-    if (!TransactNamedPipe(hPipe,
-                          &CheckVersionRequest,
-                          sizeof(CheckVersionRequest),
-                          OutBuffer,
-                          sizeof(OutBuffer),
-                          &BytesRead,
-                          NULL))
+    if (!CallNamedPipe(REWASD_PIPE_NAME,
+                       &CheckVersionRequest,
+                       sizeof(CheckVersionRequest),
+                       OutBuffer,
+                       sizeof(OutBuffer),
+                       &BytesRead,
+                       NMPWAIT_NOWAIT))
     {
         err = GetLastError();
 
-        printf("TransactNamedPipe failed, error %d!\n", err);
-
-        CloseHandle(hPipe);
+        printf("Unable to call reWASD service, error %d!\n", err);
 
         return err;
     }
 
     PREWASD_CHECK_VERSION_RESPONSE CheckVersionResponse = (PREWASD_CHECK_VERSION_RESPONSE)OutBuffer;
 
+    //Remove command tag from upper 8 bits
+    CheckVersionResponse->Size &= 0xFFFFFF;
+
     if (BytesRead < sizeof(REWASD_CHECK_VERSION_RESPONSE) ||
         CheckVersionResponse->Size < sizeof(REWASD_CHECK_VERSION_RESPONSE) ||
         CheckVersionResponse->Status != 0)
     {
-        printf("Service version response is invalid!\n");
+        printf("reWASD service response is invalid!\n");
 
-        CloseHandle(hPipe);
-
-        return err;
+        return ERROR_INVALID_DATA;
     }
 
     UCHAR MajorVersion = CheckVersionResponse->ServiceMajorVersion;
     UCHAR MinorVersion = CheckVersionResponse->ServiceMinorVersion;
 
-    printf("Service version %u.%02u.\n", MajorVersion, MinorVersion);
+    printf("reWASD service version %u.%02u.\n", MajorVersion, MinorVersion);
 
-    printf("Service flags 0x%04X.\n", CheckVersionResponse->ServiceFlags);
+    printf("reWASD service flags 0x%04X.\n", CheckVersionResponse->ServiceFlags);
 
-    if (MajorVersion < REWASD_SERVICE_FLAG_CHEATS_ENABLED_MAJOR_VERSION ||
-        (MajorVersion == REWASD_SERVICE_FLAG_CHEATS_ENABLED_MAJOR_VERSION &&
-         MinorVersion < REWASD_SERVICE_FLAG_CHEATS_ENABLED_MINOR_VERSION))
+    if (CheckVersionResponse->ServiceFlags & REWASD_SERVICE_FLAG_CHEATS_ENABLED)
     {
-        printf("Profile with turbo or combo (possible cheats) could be present. Unable to check in this version.\n");
+        printf("Some profile with turbo or combo (possible cheats) is active.\n");
     }
     else
     {
-        if (CheckVersionResponse->ServiceFlags & REWASD_SERVICE_FLAG_CHEATS_ENABLED)
+        printf("No profile with cheats is active.\n");
+    }
+
+    if (CheckVersionResponse->ServiceFlags & REWASD_SERVICE_FLAG_VIRTUAL_GAMEPAD_ENABLED)
+    {
+        //This is the case when user has created virtual gamepad of some kind.
+        printf("Some profile with virtual gamepad is active -\n");
+
+        if (CheckVersionResponse->ServiceFlags & REWASD_SERVICE_FLAG_MOUSE_TO_GAMEPAD_ENABLED)
         {
-            printf("Some profile with turbo or combo (possible cheats) is active.\n");
+            ///This flag indicates that mapping of mouse movement to virtual gamepad is enabled.
+            printf("   mouse movement mapping is present.\n");
         }
         else
         {
-            printf("No profile with cheats is active.\n");
+            printf("   mouse movement mapping is not present.\n");
         }
-    }
-
-    if (MajorVersion < REWASD_SERVICE_FLAG_VIRTUAL_AND_MOUSE_MAJOR_VERSION ||
-        (MajorVersion == REWASD_SERVICE_FLAG_VIRTUAL_AND_MOUSE_MAJOR_VERSION &&
-        MinorVersion < REWASD_SERVICE_FLAG_VIRTUAL_AND_MOUSE_MINOR_VERSION))
-    {
-        printf("Virtual gamepad and mouse mapping could be present. Unable to check in this version.\n");
     }
     else
     {
-        if (CheckVersionResponse->ServiceFlags & REWASD_SERVICE_FLAG_VIRTUAL_GAMEPAD_ENABLED)
-        {
-            //This is the case when user has created virtual gamepad of some kind.
-            printf("Some profile with virtual gamepad is active -\n");
-
-            if (CheckVersionResponse->ServiceFlags & REWASD_SERVICE_FLAG_MOUSE_TO_GAMEPAD_ENABLED)
-            {
-                ///This flag indicates that mapping of mouse movement to virtual gamepad is enabled.
-                printf("   mouse movement mapping is present.\n");
-            }
-            else
-            {
-                printf("   mouse movement mapping is not present.\n");
-            }
-        }
-        else
-        {
-            printf("No profile with virtual gamepad is active.\n");
-        }
+        printf("No profile with virtual gamepad is active.\n");
     }
 
-    CloseHandle(hPipe);
-
-    return 0;
+    return ERROR_SUCCESS;
 }
